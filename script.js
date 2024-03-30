d9a292ddbe0730710b2da5a57af3f1d9e6104b4f
blahii <blahiidima@gmail.com>
2024-03-30 10:43:01 +0100

    Update

--- script.js
+++ script.js
@@ -18,11 +18,12 @@ function fetchAndDisplayProjects(page = 1, perPage = 24, searchQuery = '', selec
 if (selectedStatus.length > 0) {
     apiURL += `&status=${encodeURIComponent(selectedStatus.join(','))}`;
 }
-    history.pushState(
-        { page: page },
-        `Page ${page}`,
-        `?page=${page}${searchQuery ? `&search=${encodeURIComponent(searchQuery)}` : ''}${selectedRegions.length > 0 ? `&regions=${encodeURIComponent(selectedRegions.join(','))}` : ''}${selectedPriorities.length > 0 ? `&priority=${encodeURIComponent(selectedPriorities.join(','))}` : ''}${selectedAreas.length > 0 ? `&area=${encodeURIComponent(selectedAreas.join(','))}` : ''}${selectedFurnishings.length > 0 ? `&furnishing=${encodeURIComponent(selectedFurnishings.join(','))}` : ''}${selectedStatus.length > 0 ? `&status=${encodeURIComponent(selectedStatus.join(','))}` : ''}`
-    );    
+history.pushState(
+    { page: page },
+    `Page ${page}`,
+    `?page=${page}${searchQuery ? `&search=${encodeURIComponent(searchQuery)}` : ''}${selectedRegions.length > 0 ? `&regions=${encodeURIComponent(selectedRegions.join(','))}` : ''}${selectedPriorities.length > 0 ? `&priority=${encodeURIComponent(selectedPriorities.join(','))}` : ''}${selectedAreas.length > 0 ? `&area=${encodeURIComponent(selectedAreas.join(','))}` : ''}${selectedFurnishings.length > 0 ? `&furnishing=${encodeURIComponent(selectedFurnishings.join(','))}` : ''}${selectedStatus.length > 0 ? `&status=${encodeURIComponent(selectedStatus.join(','))}` : ''}`
+);    
+   
     fetch(apiURL)
         .then(response => response.json())
         .then(data => {
@@ -34,15 +35,8 @@ if (selectedStatus.length > 0) {
             container.innerHTML = '';
             if (data.projects && Array.isArray(data.projects)) {
                 data.projects.forEach(project => {
-                    let priceText;
-    if (project.Price_from_AED > 0) {
-        const formattedPrice = Number(project.Price_from_AED).toLocaleString('en-US'); 
-        priceText = `AED ${formattedPrice}`;
-    } else if (project.Price_from_AED === 0 && project.Status === "Out of stock") {
-        priceText = "Out of stock";
-    } else {
-        priceText = "Contact for price";
-    }
+                    const priceText = getPriceText(project);
+                    
                     const projectCard = document.createElement('div');
                     projectCard.classList.add('project-card');
                 
@@ -72,7 +66,7 @@ if (selectedStatus.length > 0) {
                                 <div class="line-project-horizontal"></div>
                                 <div class="div-block-18">
                                     <div class="price-box">
-                                    <div class="price-value">${priceText}</div> <!-- Використання змінної priceText тут -->
+                                        <div class="price-value">${priceText}</div> <!-- Використання змінної priceText тут -->
                                     </div>
                                     <div class="line-vertically"></div>
                                     <div class="update-box">
@@ -84,103 +78,130 @@ if (selectedStatus.length > 0) {
                     `;
                     container.appendChild(projectCard);
                 });
-             const totalPages = Math.ceil(data.totalCount / perPage);
-             const pageInfoElement = document.getElementById('page-info');
-             pageInfoElement.innerHTML = `${page}/${totalPages}`; // Update the page info display
-         } else {
-             console.error('Expected projects to be an array but got:', data.projects);
-         }
-     })
-     .catch(error => {
-         console.error('Error fetching projects:', error);
+                const totalPages = Math.ceil(data.totalCount / perPage);
+                const pageInfoElement = document.getElementById('page-info');
+                pageInfoElement.innerHTML = `${page}/${totalPages}`; // Update the page info display
+            } else {
+                console.error('Expected projects to be an array but got:', data.projects);
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching projects:', error);
         });
 }
+
+function buildApiURL(page, perPage, searchQuery, selectedStatus, selectedRegions, selectedPriorities, selectedAreas, selectedFurnishings) {
+    let apiURL = `https://squid-app-bjn57.ondigitalocean.app/projects?page=${page}&perPage=${perPage}`;
+    if (searchQuery) {
+        apiURL += `&search=${encodeURIComponent(searchQuery)}`;
+    }
+    if (selectedRegions.length > 0) {
+        apiURL += `&regions=${encodeURIComponent(selectedRegions.join(','))}`;
+    }
+    if (selectedFurnishings.length > 0) {
+        apiURL += `&furnishing=${encodeURIComponent(selectedFurnishings.join(','))}`;
+    }
+    if (selectedAreas.length > 0) {
+        apiURL += `&area=${encodeURIComponent(selectedAreas.join(','))}`;
+    }
+    if (selectedPriorities.length > 0) {
+        apiURL += `&priority=${encodeURIComponent(selectedPriorities.join(','))}`;
+    }
+    if (selectedStatus.length > 0) {
+        apiURL += `&status=${encodeURIComponent(selectedStatus.join(','))}`;
+    }
+    return apiURL;
+}
+
+function getPriceText(project) {
+    let priceText;
+    if (project.Price_from_AED > 0) {
+        const formattedPrice = Number(project.Price_from_AED).toLocaleString('en-US'); 
+        priceText = `AED ${formattedPrice}`;
+    } else if (project.Price_from_AED === 0 && project.Status === "Out of stock") {
+        priceText = "Out of stock";
+    } else {
+        priceText = "Contact for price";
+    }
+    return priceText;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     let currentPage = 1;
     const perPage = 24;
     const params = new URLSearchParams(window.location.search);
     const pageFromURL = parseInt(params.get('page'), 10) || 1;
     const searchFromURL = params.get('search') || '';
-    const selectedRegions = Array.from(document.querySelectorAll('[name="regions"]:checked'))
-    .map(cb => cb.getAttribute('valueRegion'));
-    const selectedPriorities = Array.from(document.querySelectorAll('[name="priority"]:checked'))
-    .map(cb => cb.getAttribute('valuePriority'));
-    const selectedStatus = Array.from(document.querySelectorAll('[name="status"]:checked'))
-    .map(cb => cb.getAttribute('valueStatus'));
-    const selectedAreas = Array.from(document.querySelectorAll('[name="area"]:checked'))
-    .map(cb => cb.getAttribute('valueAreas'));
-     const selectedFurnishings = Array.from(document.querySelectorAll('[name="furnishing"]:checked'))
-    .map(cb => cb.getAttribute('valueFurnishings'));
-    fetchAndDisplayProjects(pageFromURL, perPage, searchFromURL, selectedRegions);
+
+    const selectedRegions = getSelectedValues('[name="regions"]');
+    const selectedPriorities = getSelectedValues('[name="priority"]');
+    const selectedStatus = getSelectedValues('[name="status"]');
+    const selectedAreas = getSelectedValues('[name="area"]');
+    const selectedFurnishings = getSelectedValues('[name="furnishing"]');
+    
+    fetchAndDisplayProjects(pageFromURL, perPage, searchFromURL, selectedStatus, selectedRegions, selectedPriorities, selectedAreas, selectedFurnishings);
+
     const prevButton = document.getElementById('prev-page');
     const nextButton = document.getElementById('next-page');
     const searchInput = document.getElementById('searchInput');
-    const searchDevInput = document.getElementById('searchInput');
-    const furnishingCheckboxes = document.querySelectorAll('[name="furnishing"]');
     const regionCheckboxes = document.querySelectorAll('[name="regions"]');
     const statusCheckboxes = document.querySelectorAll('[name="status"]');
     const priorityCheckboxes = document.querySelectorAll('[name="priority"]');
     const areaCheckboxes = document.querySelectorAll('[name="area"]');
+    const furnishingCheckboxes = document.querySelectorAll('[name="furnishing"]');
+    
     prevButton.addEventListener('click', () => {
         if (currentPage > 1) {
             currentPage -= 1;
-            const developerName = document.getElementById('developerInput') ? document.getElementById('developerInput').value : '';
-            const searchQuery = document.getElementById('searchInput') ? document.getElementById('searchInput').value : '';
-            fetchAndDisplayProjects(currentPage, perPage, searchQuery, selectedPriorities, selectedStatus, selectedRegions, developerName, selectedAreas, selectedFurnishings);
+            fetchAndDisplayProjects(currentPage, perPage, searchInput.value.trim(), selectedStatus, selectedRegions, selectedPriorities, selectedAreas, selectedFurnishings);
         }
     });
     
     nextButton.addEventListener('click', () => {
         currentPage += 1;
-        const developerName = document.getElementById('developerInput') ? document.getElementById('developerInput').value : '';
-        const searchQuery = document.getElementById('searchInput') ? document.getElementById('searchInput').value : '';
-        fetchAndDisplayProjects(currentPage, perPage, searchQuery, selectedPriorities,  selectedStatus, selectedRegions, developerName, selectedAreas, selectedFurnishings);
+        fetchAndDisplayProjects(currentPage, perPage, searchInput.value.trim(), selectedStatus, selectedRegions, selectedPriorities, selectedAreas, selectedFurnishings);
     });
+    
     searchInput.addEventListener('input', () => {
-        const searchQuery = searchInput.value.trim();
         currentPage = 1;
-        fetchAndDisplayProjects(currentPage, perPage, searchQuery, selectedPriorities, selectedStatus, selectedRegions, selectedPriorities, selectedAreas,   selectedFurnishings);
+        fetchAndDisplayProjects(currentPage, perPage, searchInput.value.trim(), selectedStatus, selectedRegions, selectedPriorities, selectedAreas, selectedFurnishings);
     });
+    
     regionCheckboxes.forEach(checkbox => {
         checkbox.addEventListener('change', () => {
-
-            const selectedRegions = Array.from(regionCheckboxes)
-                .filter(cb => cb.checked)
-                .map(cb => cb.getAttribute('valueRegion'));
-                console.log('Selected Regions:', selectedRegions);
+            const selectedRegions = getSelectedValues('[name="regions"]');
             currentPage = 1;
-            fetchAndDisplayProjects(currentPage, perPage, searchInput.value.trim(), selectedStatus, selectedRegions, selectedPriorities, selectedAreas,  selectedFurnishings);
+            fetchAndDisplayProjects(currentPage, perPage, searchInput.value.trim(), selectedStatus, selectedRegions, selectedPriorities, selectedAreas, selectedFurnishings);
         });
     });
 
     statusCheckboxes.forEach(checkbox => {
         checkbox.addEventListener('change', () => {
-
-            const selectedStatus = Array.from(statusCheckboxes)
-                .filter(cb => cb.checked)
-                .map(cb => cb.getAttribute('valueStatus'));
-                console.log('Selected Status:', selectedStatus);
+            const selectedStatus = getSelectedValues('[name="status"]');
             currentPage = 1;
-            fetchAndDisplayProjects(currentPage, perPage, searchInput.value.trim(), selectedStatus, selectedRegions, selectedPriorities, selectedAreas,  selectedFurnishings);
+            fetchAndDisplayProjects(currentPage, perPage, searchInput.value.trim(), selectedStatus, selectedRegions, selectedPriorities, selectedAreas, selectedFurnishings);
         });
     });
+    
     priorityCheckboxes.forEach(checkbox => {
         checkbox.addEventListener('change', () => {
-            const selectedPriorities = Array.from(priorityCheckboxes)
-                .filter(cb => cb.checked)
-                .map(cb => cb.getAttribute('valuePriority'));
-                console.log('Selected Priorities:', selectedPriorities);
+            const selectedPriorities = getSelectedValues('[name="priority"]');
             currentPage = 1;
-            fetchAndDisplayProjects(currentPage, perPage, searchInput.value.trim(), selectedRegions,selectedPriorities, selectedAreas,  selectedFurnishings);
+            fetchAndDisplayProjects(currentPage, perPage, searchInput.value.trim(), selectedStatus, selectedRegions, selectedPriorities, selectedAreas, selectedFurnishings);
         });
     });
-areaCheckboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', () => {
-        const selectedAreas = Array.from(areaCheckboxes)
-            .filter(cb => cb.checked)
-            .map(cb => cb.value);
-        currentPage = 1;
-        fetchAndDisplayProjects(currentPage, perPage, searchInput.value.trim(), selectedRegions, selectedPriorities, selectedAreas, selectedFurnishings);
+    
+    areaCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', () => {
+            const selectedAreas = getSelectedValues('[name="area"]');
+            currentPage = 1;
+            fetchAndDisplayProjects(currentPage, perPage, searchInput.value.trim(), selectedStatus, selectedRegions, selectedPriorities, selectedAreas, selectedFurnishings);
+        });
     });
 });
-});
+
+function getSelectedValues(selector) {
+    return Array.from(document.querySelectorAll(selector))
+        .filter(cb => cb.checked)
+        .map(cb => cb.getAttribute('value'));
+}
